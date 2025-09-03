bd7745dbcb4fe932924981803d2a5a47cf63610d
Bharath-125 <d.bharath22032004@gmail.com>
2025-09-03 11:30:34 +0530

    Add files via upload

---
+++ LabP1.java
@@ -0,0 +1,39 @@
+class LabP1{
+        public static void main(strings...args){
+               byte b=153;
+               Byte bb=b;  
+                
+                int x = 200;
+                byte b = (byte)x;
+                
+                System.out.println(Byte.MAX_VALUE);
+                System.out.println(bb.MIN_VALUE);
+                System.out.println(bb.SIZE);
+                System.out.println(Byte.BYTES);
+                System.out.println(bb.TYPE);
+                
+                short s = 15;
+                Short ss = s; 
+                
+                
+               
+                System.out.println(Short.MAX_VALUE);
+                System.out.println(ss.MIN_VALUE);
+                System.out.println(ss.TYPE);
+                System.out.println(ss.SIZE);
+                System.out.println(ss.BYTES);
+                System.out.println("-----Integer-----");
+                
+                int i = 15;
+                Integer ii = i;
+                
+                System.out.println(Integer.MAX_VALUE);
+                System.out.println(ii.MIN_VALUE);
+                System.out.println(ii.SIZE);
+                System.out.println(ii.TYPE);
+                System.out.println(ii.BYTES);
+                System.out.println("-----Integer-----");
+                          
+                
+              }
+   }
